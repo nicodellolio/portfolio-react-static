3dd447db060b01fc5725eb398862fc7da4bdcecb
nicodellolio <151004233+nicodellolio@users.noreply.github.com>
2025-10-30 15:18:50 +0100

    feat(AnalyticsTracker): add debug log for pageview tracking

--- src/components/AnalyticsTracker.jsx
+++ src/components/AnalyticsTracker.jsx
@@ -6,6 +6,7 @@ const AnalyticsTracker = () => {
   const location = useLocation();
 
   useEffect(() => {
+    console.log(`GA Pageview: ${location.pathname + location.search}`); // Log per il debug
     ReactGA.send({ hitType: "pageview", page: location.pathname + location.search });
   }, [location]);
 
